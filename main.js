940fa43da014f96964a3025817d40d70c98f8a4f
Sirazaza <89315832+Sirazaza@users.noreply.github.com>
2025-12-05 23:04:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,24 @@
-// References to DOM elements
 const popcat = document.querySelector("#popcat");
 const btn = document.querySelector("#btn");
 
-// The two images of the POP CAT
 const openMouthImg = "./images/GLE(OPEN).png";
 const closeMouthImg = "./images/GLE(CLOSE).png";
 
-// The two popping sounds
 const openMouthSound = new Audio("./sound/sound-open.mp3");
 const closeMouthSound = new Audio("./sound/sound-close.mp3");
 
-// Event Handlers (Desktop)
-btn.addEventListener("mousedown", openMouth);
-btn.addEventListener("mouseup", closeMouth);
-
-// Event Handlers (Touch Screens)
-btn.addEventListener("touchstart", function(e) {
-    e.preventDefault();
+// CLICK = เปิดปากแป๊บเดียว แล้วปิดกลับ
+btn.addEventListener("click", () => {
     openMouth();
+    setTimeout(closeMouth, 120); // 120ms = เปิดแว๊บเดียว
 });
 
-btn.addEventListener("touchend", function(e) {
-    e.preventDefault();
-    closeMouth();
-});
-
-// Switch to open mouth
 function openMouth() {
     popcat.src = openMouthImg;
     openMouthSound.currentTime = 0;
     openMouthSound.play();
 }
 
-// Switch to close mouth
 function closeMouth() {
     popcat.src = closeMouthImg;
     closeMouthSound.currentTime = 0;
